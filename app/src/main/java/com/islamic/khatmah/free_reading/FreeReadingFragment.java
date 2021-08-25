c93fc7da7524fd4dde1247cbaad1875dddeb7f9c
ahmed ward <ahmedward2212gmail.com>
2021-08-25 15:03:07 +0200

    setup the application

---
+++ app/src/main/java/com/islamic/khatmah/free_reading/FreeReadingFragment.java
@@ -0,0 +1,38 @@
+package com.islamic.khatmah.free_reading;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.islamic.khatmah.R;
+
+public class FreeReadingFragment extends Fragment {
+
+    private FreeReadingViewModel mViewModel;
+
+    public static FreeReadingFragment newInstance() {
+        return new FreeReadingFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.free_reading_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(FreeReadingViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/islamic/khatmah/progress/ProgressFragment.java
@@ -0,0 +1,38 @@
+package com.islamic.khatmah.progress;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.islamic.khatmah.R;
+
+public class ProgressFragment extends Fragment {
+
+    private ProgressViewModel mViewModel;
+
+    public static ProgressFragment newInstance() {
+        return new ProgressFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.progress_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ProgressViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
