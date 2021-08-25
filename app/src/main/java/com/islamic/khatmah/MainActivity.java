84ee8a72742cd33df7cb464c3a8bb18650f26948
ebrahim5599 <studyAndroid2020>
2021-08-25 15:27:32 +0200

    Remove Floating button

--- app/src/main/java/com/islamic/khatmah/MainActivity.java
+++ app/src/main/java/com/islamic/khatmah/MainActivity.java
@@ -32,15 +32,7 @@ public class MainActivity extends AppCompatActivity {
         new TabLayoutMediator(tabs, viewPager,
                 (tab, position) -> tab.setText(setTextOfTheTab(position))
         ).attach();
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
