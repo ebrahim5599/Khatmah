d9ae8aea6ed3fa391d137a94638e3ca182cb177b
manarelghobashy <manarelghobashy6@gmail.com>
2021-08-30 21:13:12 +0200

    manar

---
+++ app/src/main/java/com/islamic/khatmah/AlertActivity.java
@@ -0,0 +1,82 @@
+package com.islamic.khatmah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class AlertActivity extends AppCompatActivity {
+    ImageButton alarm_btn;
+    int sHour, sMinute;
+
+    TextView txt_time ;
+    Button btn_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+
+        //spinner set number of pages.
+        Spinner spinner = (Spinner) findViewById(R.id.spin_num);
+        String array[] = {"1", "2", "3", "4","5","6","7","8","9","10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AlertActivity.this, android.R.layout.simple_list_item_1,array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        txt_time = (TextView) findViewById(R.id.txt_time);
+        alarm_btn=findViewById(R.id.img_alarm);
+        alarm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    popTimePiker();
+
+
+            }
+        });
+        btn_start=findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AlertActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+    private void popTimePiker() {
+        Calendar cal = Calendar.getInstance();
+
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                cal.set(Calendar.HOUR_OF_DAY, selectedHour);
+                cal.set(Calendar.MINUTE, selectedMinute);
+                sHour = cal.get(Calendar.HOUR_OF_DAY);
+                sMinute = cal.get(Calendar.MINUTE);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh:mm aa", Locale.US);
+                String Time = simpleDateFormat.format(cal.getTime());
+                txt_time.setText(Time);
+
+
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, cal.get(Calendar.HOUR), cal.get(Calendar.MINUTE), false);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
+
+    }
+}
--- app/src/main/java/com/islamic/khatmah/MainActivity.java
+++ app/src/main/java/com/islamic/khatmah/MainActivity.java
@@ -1,19 +1,23 @@
 package com.islamic.khatmah;
 
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -25,8 +29,22 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean previouslyStarted = prefs.getBoolean(getString(R.string.pref_previously_started), false);
+        if (!previouslyStarted) {
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putBoolean(getString(R.string.pref_previously_started), Boolean.TRUE);
+            edit.apply();
+            moveToSecondary();
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this);
 
@@ -74,5 +92,10 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return "";
         }
+    }
+    public void moveToSecondary(){
+        // use an intent to travel from one activity to another.
+        Intent intent = new Intent(this,StartActivity.class);
+        startActivity(intent);
     }
 }
