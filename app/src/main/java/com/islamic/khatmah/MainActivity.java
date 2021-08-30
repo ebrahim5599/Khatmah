4340d14e801fbba83d9116e7ef9207535f13ff18
manarelghobashy <manarelghobashy6@gmail.com>
2021-08-31 01:28:26 +0200

    manar_new

--- app/src/main/java/com/islamic/khatmah/MainActivity.java
+++ app/src/main/java/com/islamic/khatmah/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             moveToSecondary();
         }
     }
-
+    ///
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
