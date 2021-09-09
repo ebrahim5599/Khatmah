bad8457d01e91c81349b9cec84f599e4f8d6d9ab
ahmed <ahmedward-eng>
2021-09-10 00:02:31 +0200

    download with intent service

--- app/src/main/java/com/islamic/khatmah/quran_activity/QuranActivity.java
+++ app/src/main/java/com/islamic/khatmah/quran_activity/QuranActivity.java
@@ -1,25 +1,9 @@
 package com.islamic.khatmah.quran_activity;
-
-import static com.islamic.khatmah.MainActivity.DATA_EXIST;
-import static com.islamic.khatmah.MainActivity.bitmaps;
-import static com.islamic.khatmah.MainActivity.pages;
-import static com.islamic.khatmah.MainActivity.sharedPreferences;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
-
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-
-import com.islamic.khatmah.MainActivity;
 import com.islamic.khatmah.R;
-
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
 
 public class QuranActivity extends AppCompatActivity {
 
@@ -34,12 +18,8 @@ public class QuranActivity extends AppCompatActivity {
         j = n.getIntExtra("PAGE_NUMBER",0);
         ViewPager2 viewPager = findViewById(R.id.viewPager);
 
-//        if(MainActivity.fileNotFound == false){
-            viewPager.setAdapter(new QuranPageAdapter(getBaseContext(), viewPager));
-//        }else{
-//            String url = "https://quran-images-api.herokuapp.com/show/page/";
-//            viewPager.setAdapter(new QuranPageAdapter(url, viewPager));
-//        }
+            viewPager.setAdapter(new QuranPageAdapter(getBaseContext()));
+
 
         viewPager.setCurrentItem(j-1, true);
     }
