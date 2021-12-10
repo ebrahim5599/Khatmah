0a55b064db91e6ea0c7ddff5965bab2da075fbe5
ebrahim5599 <studyAndroid2020>
2021-12-11 00:09:47 +0200

    BroadcastReminder

--- app/src/main/java/com/islamic/khatmah/AlarmBrodCasts/RemoveFinishDailyPortionBCR.java
+++ app/src/main/java/com/islamic/khatmah/AlarmBrodCasts/RemoveFinishDailyPortionBCR.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.widget.Toast;
 
 import com.islamic.khatmah.constants.Constant;
 
@@ -13,5 +14,6 @@ public class RemoveFinishDailyPortionBCR extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         SharedPreferences sharedPreferences = context.getSharedPreferences(Constant.MAIN_SHARED_PREFERENCES,Context.MODE_PRIVATE);
         sharedPreferences.edit().putBoolean(Constant.FINISH_DAILY_PROGRESS,false).apply();
+//        Toast.makeText(context, "لا تنسَ ورد اليوم.", Toast.LENGTH_SHORT).show();
     }
 }
